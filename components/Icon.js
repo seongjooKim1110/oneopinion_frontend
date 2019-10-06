eedd5c67ef7c6daca756a7d699ccdc16e7e66d91
Kjoobin <imkj1148@gmail.com>
2019-10-06 22:18:06 +0900

    Add home stack

---
+++ components/Icon.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Ionicons } from "@expo/vector-icons";
+import { Platform } from "@unimodules/core";
+
+const Icons = ({ name, size = "md", color = "black" }) => {
+  let os = "";
+  let iconSize = 0;
+  if (Platform.OS === "ios") {
+    os = "ios";
+  } else if (Platform.OS === "android") {
+    os = "md";
+  }
+  if (size === "sm") {
+    iconSize = 24;
+  } else if (size === "md") {
+    iconSize = 32;
+  } else {
+    iconSize = 40;
+  }
+  return <Ionicons name={`${os}-${name}`} size={iconSize} color={color} />;
+};
+
+export default Icons;
