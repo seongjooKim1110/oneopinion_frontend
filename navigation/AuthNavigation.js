c57cbe6b25a13c0c1934e1447b702c54ad4f8872
Kjoobin <imkj1148@gmail.com>
2019-11-19 17:40:16 +0900

    Modify navigation

--- navigation/AuthNavigation.js
+++ navigation/AuthNavigation.js
@@ -1,3 +1,17 @@
-import { createAppContainer, createStackNavigator } from "react-navigation";
 import React from "react";
-import Home from "../screens/Auth/AuthHome";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import AuthHome from "../screens/Auth/AuthHome";
+import SignIn from "../screens/Auth/AuthSignIn";
+
+const AuthNavigation = createStackNavigator(
+  {
+    SignIn,
+    AuthHome
+  },
+  {
+    headerMode: "none"
+  }
+);
+
+export default createAppContainer(AuthNavigation);
