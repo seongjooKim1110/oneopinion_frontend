741b36f7deb6856c4fd2b06fbf33b4391feeee01
Kjoobin <imkj1148@gmail.com>
2019-11-26 18:45:47 +0900

    Add survey 20191126

--- navigation/SurveyNavigation.js
+++ navigation/SurveyNavigation.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { Text } from "react-native";
+import { Text, Alert } from "react-native";
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import Survey from "../screens/Survey/Survey";
 import SurveyInit from "../screens/Survey/Init";
+import SurveyMake from "../screens/Survey/Make";
 import { TouchableOpacity } from "react-native-gesture-handler";
 
 const SurveyNavigation = createStackNavigator({
@@ -26,6 +27,23 @@ const SurveyNavigation = createStackNavigator({
       headerRight: (
         <TouchableOpacity onPress={() => navigation.navigate("SurveyInit")}>
           <Text>설문조사</Text>
+        </TouchableOpacity>
+      )
+    })
+  },
+  SurveyMake: {
+    screen: SurveyMake,
+    navigationOptions: ({ navigation }) => ({
+      headerLeft: (
+        <TouchableOpacity
+          onPress={() =>
+            Alert.alert("뒤로가시겠습니까?", "작성한 내용이 삭제됩니다.", [
+              { text: "YES", onPress: () => navigation.goBack(null) },
+              { text: "NO" }
+            ])
+          }
+        >
+          <Text>뒤로가기</Text>
         </TouchableOpacity>
       )
     })
